b91c29e9f3bfc8f560353f1274c329c9cf931374
mac <mac@macdeMacBook-Pro.local>
2026-09-21 23:23:58 +0800

    页脚填备案号 粤ICP备2026079874号-2，联系邮箱改 shansign.cn

--- js/components.js
+++ js/components.js
@@ -117,7 +117,7 @@ SITE.footerHTML = function () {
         '</div>' +
         '<div class="footer-bottom">' +
           '<span>© 2026 合同闪签助手 · 让每一次商务沟通，快速变成可签合同</span>' +
-          '<span>ICP 备案号：备案中 ｜ 联系：sales@flashdeal.cn</span>' +
+          '<span>ICP 备案号：<a href="https://beian.miit.gov.cn/" target="_blank" rel="noopener" style="color:inherit;">粤ICP备2026079874号-2</a> ｜ 联系：sales@shansign.cn</span>' +
         '</div>' +
       '</div>' +
     '</footer>'
